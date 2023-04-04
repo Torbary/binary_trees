ade915bd1daf7040dcb715564751d8ee992fa7ce
Torbary <torbarey020@gmail.com>
2023-04-04 02:00:22 -0700

    previous code modified

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,15 +9,19 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	if (tree)
+	{
+		int left_height = 0, right_height = 0;
 
-	if (tree == NULL)
-		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
+		if (tree->right)
+			right_height = 1 + binary_tree_height(tree->right);
+		if (tree->left)
+			left_height = 1 + binary_tree_height(tree->left);
+		if (left_height > right_height)
+			return (left_height);
+		else
+			return(right_height);
+	}
 	else
-		return (right_height + 1);
+		return (0);
 }
