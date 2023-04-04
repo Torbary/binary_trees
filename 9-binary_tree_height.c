06df658d314b07e29480cac504ddc866a69d2fb2
Torbary <torbarey020@gmail.com>
2023-04-04 02:03:03 -0700

    betty modified

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -20,7 +20,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		if (left_height > right_height)
 			return (left_height);
 		else
-			return(right_height);
+			return (right_height);
 	}
 	else
 		return (0);
