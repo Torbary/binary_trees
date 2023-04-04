19c904ccf1414aff9a5a8de449e7ae200ff005b9
Torbary <torbarey020@gmail.com>
2023-04-04 16:32:24 -0700

    modified

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,12 +13,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		int left_height = 0, right_height = 0;
+		int left_height = 1, right_height = 1;
 
 		if (tree->right)
-			right_height = 1 + binary_tree_height(tree->right);
+			right_height += binary_tree_height(tree->right);
 		if (tree->left)
-			left_height = 1 + binary_tree_height(tree->left);
+			left_height += binary_tree_height(tree->left);
 		if (left_height > right_height)
 			return (left_height);
 		else
