082597aa45559af6276dcce7515be76c8fe89b19
Torbary <torbarey020@gmail.com>
2023-04-04 15:48:45 -0700

    modified

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,12 +11,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		int left_height = 0, right_height = 0;
+		int left_height = 1, right_height = 1;
 
 		if (tree->right)
-			right_height = 1 + binary_tree_height(tree->right);
+			right_height += binary_tree_height(tree->right);
 		if (tree->left)
-			left_height = 1 + binary_tree_height(tree->left);
+			left_height += binary_tree_height(tree->left);
 		if (left_height > right_height)
 			return (left_height);
 		else
@@ -35,13 +35,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
-
 	if (tree == NULL)
 		return (0);
 
-	left_height = (int) binary_tree_height(tree->left);
-	right_height = (int) binary_tree_height(tree->right);
-
-	return (left_height - right_height);
+	return (binary_tree_height(tree->left) - 
+	        binary_tree_height(tree->right));
 }
